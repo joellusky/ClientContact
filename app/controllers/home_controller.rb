a421008f05dc0b6876523435b72754e18a0a41ac
Joel Lusky <joelzlusky@gmail.com>
2014-10-23 16:35:23 -0400

    landing page updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,7 @@ class HomeController < ApplicationController
 
     if user_signed_in?
       redirect_to dashboard_index_path
-    else
-      # render "index"
     end
-  	render :index, layout: "landing_page"
   end
 end
 
